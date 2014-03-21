0d9eb4ae15361dfd67472ee6e6acff0d7b3c31d1
Kyle Cranmer <kyle.cranmer@nyu.edu>
2014-03-21 16:37:29 -0400

    try pelican-bootstrap3 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,16 +22,22 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/kylecranmer'),
+          ('github', 'http://github.com/kylecranmer'),)
+
+CC_LICENSE="CC-BY"
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-# Additions 
 ####################################################
+# Additions 
+
+THEME = 'pelican-bootstrap3'
+#THEME = '../pelican-bootstrap3'
+#THEME = '/Users/cranmer/virtualenvs/pelican/lib/python2.7/site-packages/pelican/themes/pelican-bootstrap3'
 # This requires Pelican 3.3+
 STATIC_PATHS = ['images', 'downloads', 'favicon.png']
 
